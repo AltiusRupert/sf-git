0888f997ae72c179d738b58b6c8e5cf7d48fdb40
Rupert Barrow <AltiusRupert@users.noreply.github.com>
2018-09-12 04:55:15 +0200

    Create exp.sh

---
+++ exp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+
+sfdx login orgUsername
+#sfdx shane:mdapi:pull --all -u orgUsername
+
+#git add -A .
+#git commit -m "Commit message"
+#git push origin "branchname"
+#delete zipPath
+#delete repoPath

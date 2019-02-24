2be6e2d13612ce7133f1fd9efbf85495d6f41626
Rupert Barrow <barrow@altius-services.com>
2019-02-24 18:14:26 +0100

    Utilisation de SFDX en shell à la place de sfgit en NodeJS

--- bin/run-sfgit.js
+++ bin/run-sfgit.js
@@ -5,10 +5,24 @@
  * Or add the "Heroku Scheduler" add-on and schedule the "node bin/run-sfgit.js" 
  * command
  */
-var sfgit = require('../sfgit');
+
+/*
+ var sfgit = require('../sfgit');
 sfgit.doAll(function(err, details){
 	// Already traced in sfgit
 	//console.log(err, details);
 	
         //console.log("END : ", err ? err.error.message : '', details);
 });
+*/
+
+
+var exec = require('flex-exec');
+
+exec(['sfdx-project.sh'], function(err, out, code) {
+	if (err instanceof Error)
+	  throw err;
+	process.stderr.write(err);
+	process.stdout.write(out);
+	process.exit(code);
+      });

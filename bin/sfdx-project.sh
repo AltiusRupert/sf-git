9984435e722ef259db0e33a4d006615ef57df8c3
Rupert Barrow <AltiusRupert@users.noreply.github.com>
2019-03-01 19:13:54 +0100

    Update sfdx-project.sh

--- bin/sfdx-project.sh
+++ bin/sfdx-project.sh
@@ -59,8 +59,8 @@ echo "repo_branch__c = $6"
     # On se connecte à Salesforce
     # sf_username__c
     # sf_login_url__c
-    echo "sfdx force:auth:jwt:grant -u $sf_username__c --instanceurl $sf_login_url__c -f ./.config/opera.key -i $CLIENTID"
-          sfdx force:auth:jwt:grant -u $sf_username__c --instanceurl $sf_login_url__c -f ./.config/opera.key -i $CLIENTID
+    echo "sfdx force:auth:jwt:grant -s -u $sf_username__c --instanceurl $sf_login_url__c -f ./.config/opera.key -i $CLIENTID"
+          sfdx force:auth:jwt:grant -s -u $sf_username__c --instanceurl $sf_login_url__c -f ./.config/opera.key -i $CLIENTID
 
 
 	###############################################################
